bf7f1cd962840930b3bc9920260da8ee0fa6348f
jacqgeorg02 <92845695+jacqgeorg02@users.noreply.github.com>
2023-04-10 00:39:20 -0700

    Add files via upload

---
+++ week1/src/MyVariable1.java
@@ -0,0 +1,8 @@
+public class MyVariable1 {
+    public static void main (String[] args) {
+        String name = "Olga"; //after a statement, don't forget the semi-colon
+        int hours = 40; //we are assigning 40 to the hours variable
+        System.out.println(name); //Please note, when we have a string value, it is nested in double quotes
+        System.out.println(hours); // When we are referring to numbers/integers, no quotes
+    }
+}
