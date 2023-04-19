80143dac1a3b654edd1854955b69dcd2658616e1
jacqgeorg02 <92845695+jacqgeorg02@users.noreply.github.com>
2023-04-19 11:56:30 -0700

    Add files via upload

---
+++ week3/src/MyOperators.java
@@ -0,0 +1,61 @@
+public class MyOperators {
+    public static void main(String[] args) {
+        // Arithmetic operators
+        // employee, wage, hours worked, overtime, the whole enchilada
+
+
+//        int regHours, overtimeHours;
+//        double overtimePay, regularPay, total;
+//        float wage;
+//        double hourlyWithOvertime;
+//
+//        regHours = 40;
+//        overtimeHours = 5;
+//        wage = 25.50F;
+//        regularPay = (regHours * wage);
+//        overtimePay = (wage * 1.5) * overtimeHours;
+//        total = overtimePay + regularPay;
+//        hourlyWithOvertime = total / (regHours + overtimeHours);
+//
+//        System.out.println("Regular pay: $"+regularPay+ " dollars");
+//        System.out.println("Overtime pay: $"+overtimePay+ " dollars");
+//        System.out.println("Total pay: $"+total+ " dollars");
+//        System.out.println("Hourly Rate with Overtime: $"+hourlyWithOvertime+ " dollars");
+//        System.out.printf("$%.2f", hourlyWithOvertime);
+//        System.out.print("Hourly Rate with Overtime:");
+//        System.out.printf("$%.2f", hourlyWithOvertime);
+
+//modulus operators
+
+//int number1, number2, number3;
+//number1 = 2576;
+//number2 = 2;
+//number3 = number1 % number2;
+//if (number3==0) {
+//    System.out.println(number1 + " is an even number");
+//
+//}else {
+//    System.out.println(number1 + " is an odd number");
+//}
+////Now we are going shopping
+//        double tax, priceOfItem, taxOnItem, totalPrice;
+//            tax = .10;
+//            priceOfItem = 100.00;
+//            taxOnItem = priceOfItem * tax;
+//            totalPrice = priceOfItem + taxOnItem;
+//        System.out.println("Price of Item $" +priceOfItem+ " dollars");
+//        System.out.println("Tax on Item $" +taxOnItem+ " dollars");
+//        System.out.println("Total Price $" +totalPrice+ " dollars");
+
+        double priceOfItem;
+        priceOfItem = 100;
+        priceOfItem *=1.10;
+//        System.out.println("Total Price of Item is " +priceOfItem+ " dollars");
+        System.out.print("Total Price of Item is: ");
+        System.out.printf("$%.2f", priceOfItem);
+        System.out.print(" dollars");
+
+
+    }
+}
+
