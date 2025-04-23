8929a2def2b4c926070d8a9ae7c78bcc160c4cd8
jacqgeorg02 <92845695+jacqgeorg02@users.noreply.github.com>
2025-04-23 10:01:30 -0700

    Add files via upload

---
+++ week0/src/MyVariables.java
@@ -0,0 +1,36 @@
+public class MyVariables {
+
+    public static void main(String[] args) {
+
+        String name = "Jacquie";
+        int hours = 40;
+
+        System.out.println(name);
+        System.out.println(hours);
+
+        String tree, building, lake, ocean;
+        tree = "Japanese Maple";
+        building = "Space Needle";
+        lake = "Lake Washington";
+        ocean = "Pacific Ocean";
+
+        int weeks, decades, months, years;
+        weeks = 4;
+        decades = 10;
+        months = 3;
+        years = 5;
+
+        System.out.println(tree);
+        System.out.println(building);
+        System.out.println(lake);
+        System.out.println(ocean);
+        System.out.println(weeks);
+        System.out.println(decades);
+        System.out.println(months);
+        System.out.println(years);
+
+        System.out.println("My favorite lake is "+lake);
+
+
+    }
+}
