9647b3f2d5530a59e1c85d5edbc4fdc87f781e1d
jacqgeorg02 <92845695+jacqgeorg02@users.noreply.github.com>
2025-04-23 22:55:57 -0700

    Add files via upload

---
+++ week3/src/MyForLoop.java
@@ -0,0 +1,31 @@
+public class MyForLoop {
+
+    public static void main(String[] args) {
+
+        int count;
+
+        for(count = 0; count <= 30; count = count+3) {
+            System.out.println(count);
+        }
+        System.out.println("All Done!!");
+//        this is outside of the for loop
+
+        int newCount;
+        for(newCount = 40; newCount>0; newCount=newCount -1) {
+            System.out.println(newCount);
+
+            if(newCount<=10) {
+                System.out.println("We are almost at Blast Off! Only "+newCount+" seconds away!");
+
+            } else {
+                System.out.println(newCount);
+            }
+
+        }
+//        end of the for loop
+        System.out.println("Blast Off!!");
+
+
+
+    }
+}
